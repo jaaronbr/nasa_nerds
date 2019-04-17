408ac6fafda684dc5df2483bb7bd8406c61ae55a
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2019-04-16 21:43:24 -0600

    nc

--- spec/features/user_show_page_spec.rb
+++ spec/features/user_show_page_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'As a Logged in User' do
   it 'can see button to favorite a photo of the day' do
-    user = User.create!(first_name: 'Billy', last_name: 'Johns', password: 'password', confirmation: 'password')
+    user = User.create!(first_name: 'Billy', last_name: 'Johns', password: 'password')
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit root_path
@@ -15,7 +15,7 @@ describe 'As a Logged in User' do
   end
 
   it 'can see saved picture info printed on show page' do
-    user = User.create!(first_name: 'Billy', last_name: 'Johns', password: 'password', confirmation: 'password')
+    user = User.create!(first_name: 'Billy', last_name: 'Johns', password: 'password')
     user.nasa_pics.create!(title: "nice", description: "1", image: "www.hey.com", date_taken: "today")
     user.nasa_pics.create!(title: "okay", description: "2", image: "www.hey2.com", date_taken: "today")
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
